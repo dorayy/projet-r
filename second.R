1d47d40f5c80110c24b1a54f48041fe9eb7144ab
yarod <doray@cashflowpositif.fr>
2022-12-22 10:11:47 +0100

    tt

--- second.R
+++ second.R
@@ -23,7 +23,7 @@ library(lubridate)
 library(ggplot2)
 
 listReg <- sort(data$reg[!duplicated(data$reg)])
-dataByRegionByMonth <- foreach(i=listReg) %do% (data[data$reg == i, ] %>% group_by(month=floor_date(jour, "month"),reg=reg) %>% summarize(n_dose1_h=sum(n_dose1_h),n_dose1_f=sum(n_dose1_f)))
+dataByRegionByMonth <- foreach(i=listReg) %do% (data[data$reg == i, ] %>% group_by(month=floor_date(jour, "month"),reg=reg) %>% summarize(n_rappel_h=sum(n_rappel_h),n_rappel_f=sum(n_rappel_f)))
 
 statsDataByRegionByMonth <- list()
 find_mode <- function(x) {
@@ -32,33 +32,35 @@ find_mode <- function(x) {
   u[tab == max(tab)]
 }
 
+View()
+
 statsDataByRegionByMonth <- 
     foreach(i=1:length(dataByRegionByMonth)) %do% (list(
             region=dataByRegionByMonth[[i]]$reg[1],
-            summaryHomme=summary(dataByRegionByMonth[[i]]$n_dose1_h),
-            summaryFemme=summary(dataByRegionByMonth[[i]]$n_dose1_f),
-            modeHomme=find_mode(dataByRegionByMonth[[i]]$n_dose1_h)[1],
-            modeFemme=find_mode(dataByRegionByMonth[[i]]$n_dose1_f)[1],
-            interQuantileHomme=IQR(dataByRegionByMonth[[i]]$n_dose1_h),
-            interQuantileFemme=IQR(dataByRegionByMonth[[i]]$n_dose1_f),
-            firstDecileHomme=quantile(dataByRegionByMonth[[i]]$n_dose1_h,0.1),
-            firstDecileFemme=quantile(dataByRegionByMonth[[i]]$n_dose1_f,0.1),
-            lastDecileHomme=quantile(dataByRegionByMonth[[i]]$n_dose1_h,0.9),
-            lastDecileFemme=quantile(dataByRegionByMonth[[i]]$n_dose1_f,0.9),
-            varianceHomme=var(dataByRegionByMonth[[i]]$n_dose1_h),
-            varianceFemme=var(dataByRegionByMonth[[i]]$n_dose1_f),
-            ecarTypeHomme=sd(dataByRegionByMonth[[i]]$n_dose1_h),
-            ecarTypeFemme=sd(dataByRegionByMonth[[i]]$n_dose1_f),
-            covariance=cov(dataByRegionByMonth[[i]]$n_dose1_h,dataByRegionByMonth[[i]]$n_dose1_f),
-            corelation=cor(dataByRegionByMonth[[i]]$n_dose1_h,dataByRegionByMonth[[i]]$n_dose1_f)
+            summaryHomme=summary(dataByRegionByMonth[[i]]$n_rappel_h),
+            summaryFemme=summary(dataByRegionByMonth[[i]]$n_rappel_f),
+            modeHomme=find_mode(dataByRegionByMonth[[i]]$n_rappel_h)[1],
+            modeFemme=find_mode(dataByRegionByMonth[[i]]$n_rappel_f)[1],
+            interQuantileHomme=IQR(dataByRegionByMonth[[i]]$n_rappel_h),
+            interQuantileFemme=IQR(dataByRegionByMonth[[i]]$n_rappel_f),
+            firstDecileHomme=quantile(dataByRegionByMonth[[i]]$n_rappel_h,0.1),
+            firstDecileFemme=quantile(dataByRegionByMonth[[i]]$n_rappel_f,0.1),
+            lastDecileHomme=quantile(dataByRegionByMonth[[i]]$n_rappel_h,0.9),
+            lastDecileFemme=quantile(dataByRegionByMonth[[i]]$n_rappel_f,0.9),
+            varianceHomme=var(dataByRegionByMonth[[i]]$n_rappel_h),
+            varianceFemme=var(dataByRegionByMonth[[i]]$n_rappel_f),
+            ecarTypeHomme=sd(dataByRegionByMonth[[i]]$n_rappel_h),
+            ecarTypeFemme=sd(dataByRegionByMonth[[i]]$n_rappel_f),
+            covariance=cov(dataByRegionByMonth[[i]]$n_rappel_h,dataByRegionByMonth[[i]]$n_rappel_f),
+            corelation=cor(dataByRegionByMonth[[i]]$n_rappel_h,dataByRegionByMonth[[i]]$n_rappel_f)
         ))
 View(statsDataByRegionByMonth)
 
 foreach(i=1:length(statsDataByRegionByMonth)) %do% {
-  # plot y = n_dose1_h en fonction de x = month
+  # plot y = n_rappel_h en fonction de x = month
   plot(
     dataByRegionByMonth[[i]]$month,
-    dataByRegionByMonth[[i]]$n_dose1_h,
+    dataByRegionByMonth[[i]]$n_rappel_h,
     main=statsDataByRegionByMonth[[i]]$region,
     xlab="Mois",
     ylab="Nombre de vaccinés",
@@ -67,13 +69,15 @@ foreach(i=1:length(statsDataByRegionByMonth)) %do% {
   
   points(
     dataByRegionByMonth[[i]]$month,
-    dataByRegionByMonth[[i]]$n_dose1_f,
+    dataByRegionByMonth[[i]]$n_rappel_f,
     col="red",
     pch=19)
   
-  modele <- lm(n_dose1_f ~ poly(month,1), data=dataByRegionByMonth[[i]])
-  nextDate <- data.frame(month = as.Date(c("2023-01-01","2023-02-01")))
-  predict <- predict(modele, nextDate , interval = "confidence")
+
+  glmGauss <- glm(n_rappel_f ~ poly(month,1), data=dataByRegionByMonth[[i]])
+  new <- data.frame(month= as.Date(c("2023-01-01" , "2023-02-01")))
+  predict <- predict(glmGauss, new, interval="confidence")
+
 
   print(predict)
 }
