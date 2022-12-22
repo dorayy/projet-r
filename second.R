b5f064eee46139ee6fd396d00882a6f71808b206
yarod <doray@cashflowpositif.fr>
2022-12-22 15:00:40 +0100

    prevision rappel 3

--- second.R
+++ second.R
@@ -1,4 +1,4 @@
-#  PREVION DU NOMBRE DE VACCINE(1 er dose) EN % PAR REGION PROPORTION 2020,2021,2022 
+#  PREVION DU NOMBRE DE VACCINE(3 eme dose) EN % PAR REGION PROPORTION 2020,2021,2022 
 #  ET PREVOIR POUR 2023
 
 # set working directory
@@ -23,7 +23,7 @@ library(lubridate)
 library(ggplot2)
 
 listReg <- sort(data$reg[!duplicated(data$reg)])
-dataByRegionByMonth <- foreach(i=listReg) %do% (data[data$reg == i, ] %>% group_by(month=floor_date(jour, "month"),reg=reg) %>% summarize(n_rappel_h=sum(n_rappel_h),n_rappel_f=sum(n_rappel_f)))
+dataByRegionByMonth <- foreach(i=listReg) %do% (data[data$reg == i, ] %>% group_by(month=floor_date(jour, "month"),reg=reg) %>% summarize(n_2_rappel_h=sum(n_2_rappel_h),n_2_rappel_f=sum(n_2_rappel_f)))
 
 statsDataByRegionByMonth <- list()
 find_mode <- function(x) {
@@ -37,31 +37,32 @@ View()
 statsDataByRegionByMonth <- 
     foreach(i=1:length(dataByRegionByMonth)) %do% (list(
             region=dataByRegionByMonth[[i]]$reg[1],
-            summaryHomme=summary(dataByRegionByMonth[[i]]$n_rappel_h),
-            summaryFemme=summary(dataByRegionByMonth[[i]]$n_rappel_f),
-            modeHomme=find_mode(dataByRegionByMonth[[i]]$n_rappel_h)[1],
-            modeFemme=find_mode(dataByRegionByMonth[[i]]$n_rappel_f)[1],
-            interQuantileHomme=IQR(dataByRegionByMonth[[i]]$n_rappel_h),
-            interQuantileFemme=IQR(dataByRegionByMonth[[i]]$n_rappel_f),
-            firstDecileHomme=quantile(dataByRegionByMonth[[i]]$n_rappel_h,0.1),
-            firstDecileFemme=quantile(dataByRegionByMonth[[i]]$n_rappel_f,0.1),
-            lastDecileHomme=quantile(dataByRegionByMonth[[i]]$n_rappel_h,0.9),
-            lastDecileFemme=quantile(dataByRegionByMonth[[i]]$n_rappel_f,0.9),
-            varianceHomme=var(dataByRegionByMonth[[i]]$n_rappel_h),
-            varianceFemme=var(dataByRegionByMonth[[i]]$n_rappel_f),
-            ecarTypeHomme=sd(dataByRegionByMonth[[i]]$n_rappel_h),
-            ecarTypeFemme=sd(dataByRegionByMonth[[i]]$n_rappel_f),
-            covariance=cov(dataByRegionByMonth[[i]]$n_rappel_h,dataByRegionByMonth[[i]]$n_rappel_f),
-            corelation=cor(dataByRegionByMonth[[i]]$n_rappel_h,dataByRegionByMonth[[i]]$n_rappel_f)
+            summaryHomme=summary(dataByRegionByMonth[[i]]$n_2_rappel_h),
+            summaryFemme=summary(dataByRegionByMonth[[i]]$n_2_rappel_f),
+            modeHomme=find_mode(dataByRegionByMonth[[i]]$n_2_rappel_h)[1],
+            modeFemme=find_mode(dataByRegionByMonth[[i]]$n_2_rappel_f)[1],
+            interQuantileHomme=IQR(dataByRegionByMonth[[i]]$n_2_rappel_h),
+            interQuantileFemme=IQR(dataByRegionByMonth[[i]]$n_2_rappel_f),
+            firstDecileHomme=quantile(dataByRegionByMonth[[i]]$n_2_rappel_h,0.1),
+            firstDecileFemme=quantile(dataByRegionByMonth[[i]]$n_2_rappel_f,0.1),
+            lastDecileHomme=quantile(dataByRegionByMonth[[i]]$n_2_rappel_h,0.9),
+            lastDecileFemme=quantile(dataByRegionByMonth[[i]]$n_2_rappel_f,0.9),
+            varianceHomme=var(dataByRegionByMonth[[i]]$n_2_rappel_h),
+            varianceFemme=var(dataByRegionByMonth[[i]]$n_2_rappel_f),
+            ecarTypeHomme=sd(dataByRegionByMonth[[i]]$n_2_rappel_h),
+            ecarTypeFemme=sd(dataByRegionByMonth[[i]]$n_2_rappel_f),
+            covariance=cov(dataByRegionByMonth[[i]]$n_2_rappel_h,dataByRegionByMonth[[i]]$n_2_rappel_f),
+            corelation=cor(dataByRegionByMonth[[i]]$n_2_rappel_h,dataByRegionByMonth[[i]]$n_2_rappel_f)
         ))
 View(statsDataByRegionByMonth)
 
 foreach(i=1:length(statsDataByRegionByMonth)) %do% {
-  # plot y = n_rappel_h en fonction de x = month
+  # plot y = n_2_rappel_h en fonction de x = month
   plot(
     dataByRegionByMonth[[i]]$month,
-    dataByRegionByMonth[[i]]$n_rappel_h,
+    dataByRegionByMonth[[i]]$n_2_rappel_h,
     main=statsDataByRegionByMonth[[i]]$region,
+    xlim=c(as.Date("2020-01-01"), as.Date("2023-03-01")),
     xlab="Mois",
     ylab="Nombre de vaccinés",
     col="blue",
@@ -69,16 +70,24 @@ foreach(i=1:length(statsDataByRegionByMonth)) %do% {
   
   points(
     dataByRegionByMonth[[i]]$month,
-    dataByRegionByMonth[[i]]$n_rappel_f,
+    dataByRegionByMonth[[i]]$n_2_rappel_f,
     col="red",
     pch=19)
+
+  # regression lineaire
+  abline(lm(dataByRegionByMonth[[i]]$n_2_rappel_h ~ dataByRegionByMonth[[i]]$month), col="blue")
+
+  # predict for 2023 each month
+  modele <- lm(n_2_rappel_h ~ poly(month,1) , data=dataByRegionByMonth[[i]])
+  new <- data.frame(month=as.Date(c("2023-01-01", "2023-02-01")))
+  predict <- predict(modele, new , interval="confidence")
+  predict <- data.frame(predict)
+
+  # plot predict
+  points(new$month, predict$fit, col="yellow")
+  points(new$month, predict$lwr, col="cyan")
+  points(new$month, predict$upr, col="green")
   
 
-  glmGauss <- glm(n_rappel_f ~ poly(month,1), data=dataByRegionByMonth[[i]])
-  new <- data.frame(month= as.Date(c("2023-01-01" , "2023-02-01")))
-  predict <- predict(glmGauss, new, interval="confidence")
-
-
-  print(predict)
 }
 
